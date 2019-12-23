14100ac60320031945899ba964a109ebb28d3f3b
kylegrover <kyle@kylegrover.com>
2019-12-22 17:46:21 -0800

    make it pretty

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+    $('.photo-slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+        infinite: true,
+
+    });
+})
